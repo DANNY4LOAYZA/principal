8eeb482b116469960129f0c83d38d9a1d6a09723
DANNY4LOAYZA <dannyloayza2003@gmail.com>
2024-09-09 23:59:33 -0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+
+module.exports = {
+  mode: 'jit', // Activa el modo JIT
+  purge: ['./src/**/*.{js,jsx,ts,tsx,html}'], // Ajusta esto según la estructura de tu proyecto
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
